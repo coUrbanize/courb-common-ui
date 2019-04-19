03752f36e0ff26a8b5ea64c59da3fe80e691f9fc
Christian Spanring <cspanring@gmail.com>
2019-04-19 08:17:49 -0400

    adds tailwind addon

---
+++ addon/tailwind/config/text-sizes.js
@@ -0,0 +1,31 @@
+/*
+|-----------------------------------------------------------------------------
+| Text sizes                         https://tailwindcss.com/docs/text-sizing
+|-----------------------------------------------------------------------------
+|
+| Here is where you define your text sizes. Name these in whatever way
+| makes the most sense to you. We use size names by default, but
+| you're welcome to use a numeric scale or even something else
+| entirely.
+|
+| By default Tailwind uses the "rem" unit type for most measurements.
+| This allows you to set a root font size which all other sizes are
+| then based on. That said, you are free to use whatever units you
+| prefer, be it rems, ems, pixels or other.
+|
+| Class name: .text-{size}
+| CSS property: font-size
+|
+*/
+
+export default {
+  xs: '1rem', // 10px
+  sm: '1.2rem', // 12px
+  base: '1.4rem', // 14px
+  lg: '1.6rem', // 18px
+  xl: '2rem', // 20px
+  '2xl': '2.4rem', // 24px
+  '3xl': '3rem', // 30px
+  '4xl': '3.6rem', // 36px
+  '5xl': '4.8rem' // 48px
+};
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const buildTailwind = require('ember-cli-tailwind/lib/build-tailwind');
+const MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  config(env, baseConfig) {
+    const config = {
+      'ember-cli-tailwind': {
+        shouldIncludeStyleguide: false,
+        shouldBuildTailwind: false
+      }
+    };
+
+    const updatedConfig = Object.assign({}, baseConfig, config);
+
+    return updatedConfig;
+  },
+
+  treeForAddonStyles(tree) {
+    const trees = tree ? [tree] : [];
+    trees.push(buildTailwind(this));
+    return new MergeTrees(trees);
+  }
 };
